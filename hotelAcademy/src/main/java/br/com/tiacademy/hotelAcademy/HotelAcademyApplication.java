935efb599316988fdf90462d5a1b41b8434d7e0a
unknown <andrefranco001@gmail.com>
2022-08-12 11:36:47 -0300

    setando pastas e configurações iniciais

---
+++ hotelAcademy/src/main/java/br/com/tiacademy/hotelAcademy/HotelAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiacademy.hotelAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAcademyApplication.class, args);
+	}
+
+}
