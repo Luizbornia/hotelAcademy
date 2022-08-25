cfae58400e4a87a155cd00fa3dc8ef291a26b1fc
kassiazama <kassiazama@gmail.com>
2022-08-25 09:51:56 -0300

    atualização método post ROOM

---
+++ hotelAcademy/src/test/java/br/com/tiacademy/hotelAcademy/BookingTest.java
@@ -0,0 +1,19 @@
+package br.com.tiacademy.hotelAcademy;
+
+// @ExtendWith(MockitoExtension.class)
+public class BookingTest {
+//     @InjectMocks
+//     private BookingService bookingService;
+//     @MockBean
+//     private BookingRepository bookingRepository;
+
+// @Test
+// public void quartoExiste (){
+//     Room resultado = bookingService.createRoom(100L);
+
+
+
+// }
+
+
+}
